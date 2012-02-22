527784531964aab9f5b8450ee226542b90d4c3b1
Pieter van de Bruggen <pvande@gmail.com>
2012-02-21 18:00:40 -0800

    General performance and size refactors.

--- jason-test.sh
+++ jason-test.sh
@@ -18,12 +18,12 @@ function ok() {
 
 test="Empty Hash"
 ok '{}' '
-this: {  }
+this: { }
 '
 
 test="Empty Array"
 ok '[]' '
-this: [  ]
+this: [ ]
 '
 
 test="Array of Values"
@@ -67,8 +67,8 @@ this[0]: "JSON Test Pattern pass1"
 this[1]["object with 1 member"][0]: "array with 1 element"
 this[1]["object with 1 member"]: [ "array with 1 element" ]
 this[1]: { "object with 1 member": [ "array with 1 element" ] }
-this[2]: {  }
-this[3]: [  ]
+this[2]: { }
+this[3]: [ ]
 this[4]: -42
 this[5]: true
 this[6]: false
@@ -94,8 +94,8 @@ this[8]["hex"]: "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A"
 this[8]["true"]: true
 this[8]["false"]: false
 this[8]["null"]: null
-this[8]["array"]: [  ]
-this[8]["object"]: {  }
+this[8]["array"]: [ ]
+this[8]["object"]: { }
 this[8]["address"]: "50 St. James Street"
 this[8]["url"]: "http://www.JSON.org/"
 this[8]["comment"]: "// /* <!-- --"
@@ -119,7 +119,7 @@ this[8]["compact"]: [ 1, 2, 3, 4, 5, 6, 7 ]
 this[8]["jsontext"]: "{\"object with 1 member\":[\"array with 1 element\"]}"
 this[8]["quotes"]: "&#34; \u0022 %22 0x22 034 &#x22;"
 this[8]["\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:'\'',./<>?"]: "A key can be any string"
-this[8]: { "integer": 1234567890, "real": -9876.543210, "e": 0.123456789e-12, "E": 1.234567890E+34, "": 23456789012E66, "zero": 0, "one": 1, "space": " ", "quote": "\"", "backslash": "\\", "controls": "\b\f\n\r\t", "slash": "/ & \/", "alpha": "abcdefghijklmnopqrstuvwyz", "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ", "digit": "0123456789", "0123456789": "digit", "special": "`1~!@#$%^&*()_+-={'\'':[,]}|;.</>?", "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A", "true": true, "false": false, "null": null, "array": [  ], "object": {  }, "address": "50 St. James Street", "url": "http://www.JSON.org/", "comment": "// /* <!-- --", "# -- --> */": " ", " s p a c e d ": [ 1, 2, 3, 4, 5, 6, 7 ], "compact": [ 1, 2, 3, 4, 5, 6, 7 ], "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}", "quotes": "&#34; \u0022 %22 0x22 034 &#x22;", "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:'\'',./<>?": "A key can be any string" }
+this[8]: { "integer": 1234567890, "real": -9876.543210, "e": 0.123456789e-12, "E": 1.234567890E+34, "": 23456789012E66, "zero": 0, "one": 1, "space": " ", "quote": "\"", "backslash": "\\", "controls": "\b\f\n\r\t", "slash": "/ & \/", "alpha": "abcdefghijklmnopqrstuvwyz", "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ", "digit": "0123456789", "0123456789": "digit", "special": "`1~!@#$%^&*()_+-={'\'':[,]}|;.</>?", "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A", "true": true, "false": false, "null": null, "array": [ ], "object": { }, "address": "50 St. James Street", "url": "http://www.JSON.org/", "comment": "// /* <!-- --", "# -- --> */": " ", " s p a c e d ": [ 1, 2, 3, 4, 5, 6, 7 ], "compact": [ 1, 2, 3, 4, 5, 6, 7 ], "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}", "quotes": "&#34; \u0022 %22 0x22 034 &#x22;", "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:'\'',./<>?": "A key can be any string" }
 this[9]: 0.5
 this[10]: 98.6
 this[11]: 99.44
@@ -131,5 +131,5 @@ this[16]: 1e00
 this[17]: 2e+00
 this[18]: 2e-00
 this[19]: "rosebud"
-this: [ "JSON Test Pattern pass1", { "object with 1 member": [ "array with 1 element" ] }, {  }, [  ], -42, true, false, null, { "integer": 1234567890, "real": -9876.543210, "e": 0.123456789e-12, "E": 1.234567890E+34, "": 23456789012E66, "zero": 0, "one": 1, "space": " ", "quote": "\"", "backslash": "\\", "controls": "\b\f\n\r\t", "slash": "/ & \/", "alpha": "abcdefghijklmnopqrstuvwyz", "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ", "digit": "0123456789", "0123456789": "digit", "special": "`1~!@#$%^&*()_+-={'\'':[,]}|;.</>?", "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A", "true": true, "false": false, "null": null, "array": [  ], "object": {  }, "address": "50 St. James Street", "url": "http://www.JSON.org/", "comment": "// /* <!-- --", "# -- --> */": " ", " s p a c e d ": [ 1, 2, 3, 4, 5, 6, 7 ], "compact": [ 1, 2, 3, 4, 5, 6, 7 ], "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}", "quotes": "&#34; \u0022 %22 0x22 034 &#x22;", "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:'\'',./<>?": "A key can be any string" }, 0.5, 98.6, 99.44, 1066, 1e1, 0.1e1, 1e-1, 1e00, 2e+00, 2e-00, "rosebud" ]
+this: [ "JSON Test Pattern pass1", { "object with 1 member": [ "array with 1 element" ] }, { }, [ ], -42, true, false, null, { "integer": 1234567890, "real": -9876.543210, "e": 0.123456789e-12, "E": 1.234567890E+34, "": 23456789012E66, "zero": 0, "one": 1, "space": " ", "quote": "\"", "backslash": "\\", "controls": "\b\f\n\r\t", "slash": "/ & \/", "alpha": "abcdefghijklmnopqrstuvwyz", "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ", "digit": "0123456789", "0123456789": "digit", "special": "`1~!@#$%^&*()_+-={'\'':[,]}|;.</>?", "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A", "true": true, "false": false, "null": null, "array": [ ], "object": { }, "address": "50 St. James Street", "url": "http://www.JSON.org/", "comment": "// /* <!-- --", "# -- --> */": " ", " s p a c e d ": [ 1, 2, 3, 4, 5, 6, 7 ], "compact": [ 1, 2, 3, 4, 5, 6, 7 ], "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}", "quotes": "&#34; \u0022 %22 0x22 034 &#x22;", "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:'\'',./<>?": "A key can be any string" }, 0.5, 98.6, 99.44, 1066, 1e1, 0.1e1, 1e-1, 1e00, 2e+00, 2e-00, "rosebud" ]
 '
